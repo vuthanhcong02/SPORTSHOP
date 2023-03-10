784ed4205d6629a5e38bf794a5cf2e5c6b41502c
Vũ Thành Công <104372944+vuthanhcong02@users.noreply.github.com>
2023-03-10 17:52:00 +0700

    chage css and build page cart

--- sport-shop/src/pages/Cart/Cart.js
+++ sport-shop/src/pages/Cart/Cart.js
@@ -1,9 +1,31 @@
+import './Cart.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import routeConfig from '../../config/routes';
 function Cart() {
-    return ( 
-        <div>
-            <h1>Cart</h1>
+    const CartItem = false;
+    return (
+        <div className="container mt-3">
+            <div className="p-4 text-center border mt-2">
+                <h1>Giỏ hàng</h1>
+                <h4>Store</h4>
+            </div>
+            {!CartItem ? (
+                <>
+                    <div className="p-5 mt-3 pt-3 border text-center d-flex flex-column justify-content-center align-items-center">
+                        <FontAwesomeIcon className="img-fluid" icon={faCartShopping} />
+                        <span className="mt-2 fs-5 fw-medium">Bạn chưa có sản phẩm nào trong giỏ hàng</span>
+                        <Link to={routeConfig.product} className="btnGoto mt-4 text-center fs-5 text-decoration-none">
+                            Đi đến cửa hàng
+                        </Link>
+                    </div>
+                </>
+            ) : (
+                <></>
+            )}
         </div>
     );
 }
 
-export default Cart
+export default Cart;
--- sport-shop/src/pages/Product/Product.js
+++ sport-shop/src/pages/Product/Product.js
@@ -6,58 +6,56 @@ function Product() {
     return ( 
         <div className=" wrapper container mt-3">
             <div className="row">
-                <div className="col-3 p-3 m-2 ">
+                <div className="col-3 p-3 m-2">
                     <div className='d-flex justify-content-between pb-2'>
-                        <span className="fs-6">Bo loc</span>
-                        <span className="fs-6">Xoa tat ca</span>
+                        <span className="fs-6 fw-bold">Bộ lọc</span>
+                        <span className="fs-6 fw-bold">Xóa tất cả</span>
                     </div>
                     <hr/>
-                    <div className="d-flex justify-content-between pt-4">
-                        <span className="fs-6">Gia tien : </span>
-                        <select className="w-75">
-                            <option className="col" value="">500 VND</option>
+                    <div className="d-flex justify-content-between align-items-center pt-4">
+                        <span className="fs-6 fw-bold">Giá tiền : </span>
+                        <select className="w-75 p-2">
+                            <option value="">500 VND</option>
                             <option value="">1tr VND</option>
                             <option value="">1tr VND-1tr5 VND</option>
                             <option value="">2tr tro len</option>
                         </select>
                     </div>
-                    <span className="d-flex pt-4 pb-2 fs-5">Ap dung</span>
+                    <span className="d-flex pt-4 pb-2 fs-5">Áp dụng</span>
                     <hr/>
                     <div className="">
-                        <span className="fs-4">Phan loai</span>
-                        <div class="form-check pt-3 ">
-                            <input class="form-check-input text-bg-secondary border-secondary" type="checkbox" id="check1" name="option1" value="something" />
-                            <label class="form-check-label">Option 1</label>
+                        <span className="fs-4 fw-bold">Phân loại</span>
+                        <div className="form-check pt-3 ">
+                            <input className="form-check-input text-bg-secondary border-secondary" type="checkbox" id="check1" name="option1" value="something" />
+                            <label className="form-check-label">Option 1</label>
                         </div>
-                        <div class="form-check pt-3">
-                            <input class="form-check-input text-bg-secondary border-secondary" type="checkbox" id="check1" name="option1" value="something" />
-                            <label class="form-check-label">Option 1</label>
+                        <div className="form-check pt-3">
+                            <input className="form-check-input text-bg-secondary border-secondary" type="checkbox" id="check1" name="option1" value="something" />
+                            <label className="form-check-label">Option 1</label>
                         </div>
-                        <div class="form-check pt-3">
-                            <input class="form-check-input text-bg-secondary border-secondary" type="checkbox" id="check1" name="option1" value="something" />
-                            <label class="form-check-label">Option 1</label>
+                        <div className="form-check pt-3">
+                            <input className="form-check-input text-bg-secondary border-secondary" type="checkbox" id="check1" name="option1" value="something" />
+                            <label className="form-check-label">Option 1</label>
                         </div>
-                        <div class="form-check pt-3">
-                            <input class="form-check-input text-bg-secondary border-secondary" type="checkbox" id="check1" name="option1" value="something"/>
-                            <label class="form-check-label">Option 1</label>
+                        <div className="form-check pt-3">
+                            <input className="form-check-input text-bg-secondary border-secondary" type="checkbox" id="check1" name="option1" value="something"/>
+                            <label className="form-check-label">Option 1</label>
                         </div>
 
                     </div>
                 </div>
                 <div className="col-8 p-3 m-1">
                     <div className="d-flex justify-content-between">
-                        <span className="fs-4">Hien thi san pham</span>
+                        <span className="fs-5 fw-bold">Hiện thị sản phẩm</span>
                         <div className="btn-search">
-                            <input className="search" type="text" placeholder="Tim kiem..."/>
+                            <input className="search" type="text" placeholder="Tìm kiếm..."/>
                             <FontAwesomeIcon  className="close" icon={faSearch}/>
                         </div>
+                       
                     </div>
+                    <hr className="hr"/>
                     {/* <div className="m-3 p-5 pt-3 border">
                     </div> */}
-                    <ProductItem/>
-                    <hr/>
-                    <ProductItem/>
-                    <hr/>
                     <ProductItem/>
                 </div>
             </div>
