6713af30cc95be594f2dcbd7501ad4b358e823b2
Vũ Thành Công <104372944+vuthanhcong02@users.noreply.github.com>
2023-03-20 11:43:55 +0700

    demo contact about page

--- sport-shop/src/pages/About/About.js
+++ sport-shop/src/pages/About/About.js
@@ -1,7 +1,29 @@
+import './About.css'
 function About() {
     return ( 
-        <div>
-            About pages
+        <div className="container p-2">
+            <div className="title p-3 border">
+                <h1 className="text-center p-3">About US</h1>
+            </div>
+            <div className="content p3">
+                <div className="bg-img row d-flex justify-content-center">
+                    <div className="col d-flex border">
+                        <img className="img-fluid object-fit-cover h-100 w-75" src="https://msmobile.com.vn/upload_images/images/hinh-nen-powerpoint-dep-nhat-2017-1.jpg" alt=""/>
+                    </div>
+                    <div className="col p-3 border">
+
+                    </div>
+                </div>
+                <div className="row">
+                        <div className="col">
+                            <h5>Thach thuc</h5>
+                            <span>abcxyz</span>
+                        </div>
+                        <div className="col">
+
+                        </div>
+                    </div>
+            </div>
         </div>
     );
 }
--- sport-shop/src/pages/Contact/Contact.js
+++ sport-shop/src/pages/Contact/Contact.js
@@ -1,7 +1,71 @@
+import './Contact.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faLocationDot, faMobileScreen } from '@fortawesome/free-solid-svg-icons';
 function Contact() {
     return ( 
-        <div>
-            Contact pages
+        <div className="row d-flex justify-content-center">
+            <div className="col-3 mt-3 contact-logo">
+                <div className="mt-4">
+                    <img className="img-fluid w-100" src="https://geography-vnu.edu.vn/wp-content/uploads/wordpress/tieng-anh-van-phong-thong-dung-nhat-1.jpg" alt=""/>
+                </div>
+                <div className="address">
+                    <div className="mt-3 d-flex justify-content-start align-items-center">
+                         <div className="location-icon">
+                            <FontAwesomeIcon icon={faLocationDot}/>
+                        </div >
+                        <span className="text">Tòa nhà A,phường B,quận C,Hà Nội</span>    
+                    </div>
+                    <div className="mt-3  d-flex justify-content-start align-items-center">
+                      <div className="phone-icon">
+                            <FontAwesomeIcon icon={faMobileScreen}/>
+                     </div >
+                        <span className="text">0971765824</span>    
+                    </div>
+                    <div className="mt-3 d-flex justify-content-start align-items-center">
+                       <div className="mail-icon ">
+                            <FontAwesomeIcon icon={faEnvelope}/>  
+                        </div > 
+                        <a href="https://mail.google.com/"className="text">congvtc02@gmail.com</a>    
+                    </div>
+                </div>
+            </div>
+            <div className="col-7  mt-3">
+                <div className="contact-main">
+                    <div className="mt-4">
+                        <span className="">Chúng tôi rất mong muốn lắng nghe ý kiến của quý khách.Vui lòng gửi mọi yêu cầu ,<br/> mọi thắc mắc theo thông tin bên dưới.Chúng tôi sẽ liên lạc với bạn sớm nhất có thể.</span>
+                    </div>
+                    <div className="mb-3 mt-3">
+                        <label for="exampleFormControlInput1" className="form-label">HỌ VÀ TÊN</label>
+                        <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Enter your name"/>
+                    </div>
+                    <div className="mb-3 mt-3">
+                        <label for="exampleFormControlInput1" className="form-label">EMAIL</label>
+                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                    </div>
+                    <div className="mb-3 mt-3">
+                        <label for="exampleFormControlInput1" className="form-label">ĐIỆN THOẠI</label>
+                        <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Enter your phone"/>
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleFormControlTextarea1" className="form-label">NỘI DUNG</label>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    <div >
+                        <button className="btn btn-outline-danger w-100">Gửi</button>
+                    </div>
+                </div>
+            </div>
+            <div className="footer-contact d-flex justify-content-between p-3 m-4 align-items-center row ">
+               <di className="title-footer-contact col">
+                    <h4>
+                        ĐĂNG KÍ NHẬN KHUYẾN MÃI
+                    </h4>
+               </di>
+               <div className="title-footer-contact col d-flex justify-content-center align-items-center">
+                    <input type="text" placeholder="Nhập email của bạn"/>
+                    <button className="btn btn-outline-danger">Gửi</button>
+               </div>
+            </div>
         </div>
     );
 }
